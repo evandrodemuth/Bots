6b28a83ba9a7dbe5ab6cc7bcd7e8e7821d844378
Evandro Guimarães Demuth <evandrodemuth@gmail.com>
2016-10-26 01:13:35 -0200

    My personal bots
    This repository is for my personal Bots, created to be useful and to
    help lazy people like me.

---
+++ ip.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+wget -q 'http://ipinfo.io/ip' -O-
---
+++ myTelegramBot.py
@@ -0,0 +1,42 @@
+import time
+import random
+import datetime
+import telepot
+from CommandParser import CommandParser
+
+"""
+```
+- `/roll` - reply with a random integer between 1 and 6, like rolling a dice.
+- `/time` - reply with the current time, like a clock.
+"""
+
+def handle(msg):
+    chat_id = msg['chat']['id']
+    command = msg['text']
+
+    print 'Got command: %s' % command
+
+    if command == '/roll':
+        bot.sendMessage(chat_id, random.randint(1,6))
+    elif command == '/time':
+        bot.sendMessage(chat_id, str(datetime.datetime.now()))
+    elif command == '/info':
+	bot.sendMessage(chat_id, 'I am a bot. My name is DemuthBot')
+    elif command == '/uptime':
+      	 bot.sendMessage(chat_id, cp.runAndReturn(cp.getCommand("load")))
+    elif command == '/ip':
+         bot.sendMessage(chat_id, cp.runAndReturn(cp.getCommand("publicIP")))
+    elif command == '/zemuniz':
+  	bot.sendMessage(chat_id, 'Um musico brasileiro porreta!')
+
+
+
+
+cp = CommandParser()
+bot = telepot.Bot('250247563:AAFkUm0262nysZhMHjPv4IoR5ro46GeZ3Kc')
+bot.message_loop(handle)
+print 'I am listening ...'
+
+while 1:
+    time.sleep(10)
+
